cc01b75611c06ee327a4581369c1e7853a974479
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-10-24 20:13:48 +0300

    feat:  getJSON function added

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ canvas.height = 512;
 const cellSize = 128;
 
 const ctx = canvas.getContext('2d');
+
+const canvas4 = {
+    path: 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json',
+    content: null
+}
+
+const canvas32 = {
+    path: 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json',
+    content: null
+}
+
+
+function getJSON(object) {
+    fetch(path)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            if (object.content === null) {
+                object.content = data;
+            }
+
+        })
+}
