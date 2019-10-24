f4d43229d0ecb5ced258a1676b545125d936c312
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-10-24 20:42:16 +0300

    feat: getJSON function correted

--- script.js
+++ script.js
@@ -19,29 +19,28 @@ const canvas32 = {
 
 
 function getJSON(object) {
-    fetch(object.path)
-        .then(function (resp) {
-            return resp.json();
-        })
-        .then(function (data) {          
-            if (object.content === null) {
+    if (object.content === null) {
+        fetch(object.path)
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (data) {
                 object.content = data;
-            }
-
-        })
+            })
+    }
 }
 
 const button4 = document.querySelector('.btn4');
-function fillCanvas (object){
+function fillCanvas(object) {
     getJSON(object);
-    return function(){
+    return function () {
         const scale = 512 / object.content.length;
-        for(let row = 0,len = object.content.length; row < len; row++){
-            for(let col = 0,len=object.content.length;col<len;col++){
-                if(object.content.length ===4){
-                    ctx.fillStyle = '#'+object.content[row][col];
+        for (let row = 0, len = object.content.length; row < len; row++) {
+            for (let col = 0, len = object.content.length; col < len; col++) {
+                if (object.content.length === 4) {
+                    ctx.fillStyle = '#' + object.content[row][col];
                 }
-                ctx.fillRect(row*scale, col*scale, scale, scale);                
+                ctx.fillRect(row * scale, col * scale, scale, scale);
             }
         }
     }
